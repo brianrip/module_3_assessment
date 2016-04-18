03a78c66c34e764ad21933e3dbc3e4e75c60c7eb
Brian Rippeto <rippeto@gmail.com>
2016-04-18 10:21:19 -0600

    Find base url for api endpoint unsuccessful

--- app/models/location.rb
+++ app/models/location.rb
@@ -2,7 +2,7 @@ class Location < OpenStruct
 
   def self.data(location_info)
     station = Location.new
-    station.name          = location_info.name
+    stationLo.name          = location_info.name
     station.address       = location_info.address
     station.fuel_types    = location_info.fuel_types
     station.distance      = location_info.distance
--- spec/services/location_services_spec.rb
+++ spec/services/location_services_spec.rb
@@ -1,12 +1,12 @@
-describe ActivityService do
+describe LocationService do
   context '#locations' do
     it 'returns locations' do
       VCR.use_cassette 'location_service#locations' do
         service = LocationService.new
         locations = service.location(name: "")
         activity = location.first
-        expect(location[:name]).to eq(
-        expect(location.count).to eq(30)
+        expect(location[:name]).to eq()
+        expect(location.count).to eq()
       end
     end
   end
