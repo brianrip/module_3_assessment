d8772d937d52c0fd1c8118b4b63408a73fc4a35e
Brian Rippeto <rippeto@gmail.com>
2016-04-18 09:55:20 -0600

    Add location and station variables to welcome controller in index action

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,12 @@
 class WelcomeController < ApplicationController
   def index
-    
+    @stations = location.all
   end
+
+private
+
+  def location
+    @location ||= LocationService.new
+  end
+
 end
